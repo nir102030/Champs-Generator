82611391fdab14cbc5d22461c50937a4cf1d4457
nir102030 <nir102030@gmail.com>
2021-08-03 10:09:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const imgSources = [
+  "Artium.jpg",
+  "Avishag.jpg",
+  "Sagi.jpg",
+  "Timna.jpg",
+  "Arik.jpg",
+  "Yarden.jpg",
+  "Gal.jpg",
+  "Ori.jpg",
+];
+const assetsBasePath = "./assets/";
+let currentImg = "Artium.jpg";
+const $currentImgEl = document.getElementById("my-img");
+
+function generateImg() {
+  const nextImg = getNewImg(currentImg);
+  currentImg = nextImg;
+  $currentImgEl.src = assetsBasePath + nextImg;
+  document.title = currentImg.substring(0, currentImg.indexOf("."));
+}
+
+function getNewImg(currentImg) {
+  const currentImgIndx = imgSources.indexOf(currentImg);
+  if (currentImgIndx == imgSources.length - 1) {
+    return imgSources[0];
+  }
+  return imgSources[currentImgIndx + 1];
+}
+
+// handle button click
+document.getElementById("button").addEventListener("click", generateImg);
